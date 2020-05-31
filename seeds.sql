7b148de4f764703eb9d9c597f26e94c71c870f47
Dan Lyons <danielclyons@yahoo.com>
2020-05-31 15:22:54 -0700

    full functional

--- seeds.sql
+++ seeds.sql
@@ -7,13 +7,13 @@ VALUES
 
 INSERT INTO roles (title, salary, department_id)
 VALUES
-('Salesperson', '80000', '1' ),
+('Sales Person', '80000', '1' ),
 ('Sales Lead', '100000', '1'),
 ('Lead Engineer', '150000' ,'2'),
 ('Software Engineer', '120000', '2'),
 ('Legal Team Lead', '250000', '4'),
-('Lawyer', '190000', '4'),
-('Accountant','125000', '3' );
+('Lawyer ', '190000', '4'),
+('Accountant ','125000', '3' );
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
