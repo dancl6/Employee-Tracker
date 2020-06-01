338284397ee8d458a91ae8348b4974ad8c9c6152
Dan Lyons <danielclyons@yahoo.com>
2020-05-31 17:55:54 -0700

    full functional comments and README

--- server.js
+++ server.js
@@ -8,28 +8,6 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-
-
-
-// simple query
-// connection.query(
-//   'SELECT employee.id, first_name, last_name, title, dept_name, salary, manager_id FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id',
-//   function(err, results, fields) {
-//     console.log(results); // results contains rows returned by server
-//     console.log(fields); // fields contains extra meta data about results, if available
-//   }
-// );
- 
-// with placeholder
-// connection.query(
-//   'SELECT employee.id, first_name, last_name, title, dept_name, salary, manager_id FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id',
-//   // ['Page', 45],
-//   function(err, results) {
-//     console.log(results);
-//   }
-// );
-// Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
   res.status(404).end();
 });
